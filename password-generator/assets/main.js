1bd97b5c3ce45372633a61abeced2607ea514d49
vcollins1 <vontrellecollins@gmail.com>
2024-06-19 22:31:26 -0400

    javascript implementation for password options

--- password-generator/assets/main.js
+++ password-generator/assets/main.js
@@ -1,3 +1,12 @@
+let pwLength = 0;
+const passwordOptions = {
+  "upper": false,
+  "lower": false,
+  "numbers": false,
+  "symbols": false,
+};
+let optionCount = 0;
+
 document.addEventListener("input", e => {
   if (e.target.classList.contains("slider--input")) {
     let percentage =  (e.target.value / e.target.max) * 100;
@@ -5,6 +14,35 @@ document.addEventListener("input", e => {
         linear-gradient(to right, var(--neon) ${percentage}%, var(--very-dark-gray) 0%)
     `;
 
-    document.querySelector(".slider--value").innerText = e.target.value;
+    pwLength = e.target.value;
+    document.querySelector(".slider--value").innerText = pwLength;
+  }
+});
+
+document.addEventListener("click", e => {
+  if (e.target.classList.contains("option__input")) {
+    if (e.target.checked) {
+      optionCount++;
+      if (e.target.classList.contains("option-1"))
+        passwordOptions["upper"] = true;
+      else if (e.target.classList.contains("option-2"))
+        passwordOptions["lower"] = true;
+      else if (e.target.classList.contains("option-3"))
+        passwordOptions["numbers"] = true;
+      else
+        passwordOptions["symbols"] = true;
+    } 
+    
+    else {
+      optionCount--;
+      if (e.target.classList.contains("option-1"))
+        passwordOptions["upper"] = false;
+      else if (e.target.classList.contains("option-2"))
+        passwordOptions["lower"] = false;
+      else if (e.target.classList.contains("option-3"))
+        passwordOptions["numbers"] = false;
+      else
+        passwordOptions["symbols"] = false;
+    }
   }
 });
