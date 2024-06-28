70f698ed7e6f3e9b68984bc0791ab7d3f1e8de37
vcollins1 <vontrellecollins@gmail.com>
2024-06-28 13:14:05 -0400

    javascript code refactor

--- frontend-quiz/assets/main.js
+++ frontend-quiz/assets/main.js
@@ -1,51 +1,33 @@
+// Read quiz data from json file
 const response = await fetch("../data.json");
 const data = await response.json();
-const quizzes = data["quizzes"];
-const optionsLetters = ['A', 'B', 'C', 'D'];
+const quizzes = data["quizzes"]; // quizzes object
+
+// html controlled elements
 const questionH1 = document.querySelector(".section-info__header");
 const currentQuestion = document.querySelector(".section-info__choice");
 const btn = document.querySelector(".btn");
 const progressValue = document.querySelector(".progress__value");
 const quizHeader = document.querySelector(".quiz-header");
 
+// code global variables
 let quiz = {};
-let questionCount = 0;
+let questionCount = 0
 let maxQuestions = 0;
 let screen = "start";
 let questions = [];
 let question = "";
 let options = [];
 let curTarget = undefined;
+const optionsLetters = ['A', 'B', 'C', 'D'];
 
 let optionSelected = undefined;
 let correct = 0;
 
 let quizSelected = undefined;
-let ansSelected = undefined; 
+let ansSelected = undefined;
 
-const escapeHTML = str =>
-  str.replace(
-    /[&<>'"]/g,
-    tag =>
-      ({
-        '&': '&amp;',
-        '<': '&lt;',
-        '>': '&gt;',
-        "'": '&#39;',
-        '"': '&quot;'
-      }[tag] || tag)
-  );
-
-quizzes.forEach((quiz, i) => {
-  const box = document.querySelector(`.box--${i+1}`);
-  const html = `
-    <div class="box__image ${quiz["title"].toLowerCase()}"><img src=${quiz["icon"]} alt=${quiz["title"]}></div>
-    <div class="box__text">${quiz["title"]}</div>
-  `;
-
-  box.innerHTML = html;
-});
-
+populateStartMenu();
 
 document.addEventListener("click", e => {
   if (e.target.classList.contains("toggle__switch")) {
@@ -66,50 +48,21 @@ document.addEventListener("click", e => {
     if (screen == "start") {
       quizSelected = e.target.parentElement;
       screen = "quizStart";
-      
       document.querySelector(".progress").style.display = "block";
       document.querySelector(".section-info").classList.add("quiz-select");
-    
-      if (quizSelected.classList.contains("box--1"))
-        quiz = quizzes[0];
-      else if (quizSelected.classList.contains("box--2"))
-        quiz = quizzes[1];
-      else if (quizSelected.classList.contains("box--3"))
-        quiz = quizzes[2];
-      else if (quizSelected.classList.contains("box--4"))
-        quiz = quizzes[3];
-
+      quiz = quizzes[quizSelected.classList[1].slice(-1) - 1];
+      quizHeader.innerHTML = `
+        <img src="${quiz["icon"]}" class="${quiz["title"].toLowerCase()}">
+        <span>${quiz["title"]}</span>
+      `;  
       questions = quiz["questions"];
       maxQuestions = questions.length;
-
       question = questions[questionCount];
-      options = question["options"];
 
-      questionH1.classList.add("quiz-question");
-      questionH1.textContent = question["question"];
-
-      currentQuestion.innerHTML = `Question ${questionCount + 1} of ${maxQuestions};`;
-
-      options.forEach((option, i) => {
-        const box = document.querySelector(`.box--${i+1}`);
-        const html = `
-          <div class="box__image box__image--option">${optionsLetters[i]}</div>
-          <div class="box__text">${escapeHTML(option)}</div>
-          <img class="result result--correct" src="./assets/images/icon-correct.svg">
-          <img class="result result--error" src="./assets/images/icon-error.svg">
-        `;
-
-        box.innerHTML = html;
-      });
-
-      quizHeader.innerHTML = `
-      <img src="${quiz["icon"]}" class="${quiz["title"].toLowerCase()}">
-      <span>${quiz["title"]}</span>
-      `;
+      updateQuestionPage(question);
 
       btn.textContent = "Submit Answer";
       btn.style.visibility = "visible";
-      progressValue.style.width = `${((questionCount+1)/maxQuestions) * 100}%`;
     }
 
     else if (screen === "quizStart" && btn.textContent == "Submit Answer") {
@@ -129,14 +82,7 @@ document.addEventListener("click", e => {
         document.querySelector(".no-selection").style.visibility = "visible";
       }
       else {
-        if (ansSelected.classList.contains("box--1"))
-          optionSelected = 0;
-        else if (ansSelected.classList.contains("box--2"))
-          optionSelected = 1;
-        else if (ansSelected.classList.contains("box--3"))
-          optionSelected = 2;
-        else if (ansSelected.classList.contains("box--4"))
-          optionSelected = 3;
+        optionSelected = ansSelected.classList[1].slice(-1) - 1;
     
         if (options[optionSelected] === question["answer"]) {
           ansSelected.classList.add("correct");
@@ -159,68 +105,22 @@ document.addEventListener("click", e => {
 
     else if (btn.textContent == "Next Question") {
       ansSelected = undefined;
-      progressValue.style.width = `${((questionCount+1)/maxQuestions) * 100}%`; 
       question = questions[questionCount];
-      options = question["options"];
 
-      questionH1.classList.add("quiz-question");
-      questionH1.textContent = question["question"];
+      updateQuestionPage(question); 
 
-      currentQuestion.innerHTML = `Question ${questionCount + 1} of ${maxQuestions};`;
-
-      options.forEach((option, i) => {
-        const box = document.querySelector(`.box--${i+1}`);
-        const html = `
-          <div class="box__image box__image--option">${optionsLetters[i]}</div>
-          <div class="box__text">${escapeHTML(option)}</div>
-          <img class="result result--correct" src="./assets/images/icon-correct.svg">
-          <img class="result result--error" src="./assets/images/icon-error.svg">
-        `;
-
-        box.innerHTML = html;
-      });
       document.querySelector(".selected").classList.remove("correct", "error", "selected");
-
       btn.textContent = "Submit Answer";
     }
 
     else if (btn.textContent == "View Results") {
-      document.querySelector(".selected").classList.remove("correct", "error", "selected");
-
-      document.querySelector(".progress").style.display = "none";
-      currentQuestion.style.display = "none";
-      questionH1.innerHTML = `
-        Quiz completed<span class="section-info__header--span">You scored...</span>
-      `;
-      questionH1.classList.remove("quiz-question");
-
-      document.querySelectorAll(".box").forEach(box => {
-        box.innerHTML = "";
-        box.style.display = "none";
-      });
-
-      const resultBox = document.querySelector(".box--1");
-      resultBox.style.display = "flex";
-      resultBox.classList.add("box--result");
-      
-      const html = `
-        <div class="quiz-header">
-          <img src="${quiz["icon"]}" class="${quiz["title"].toLowerCase()}">
-          <span>${quiz["title"]}</span>
-        </div>
-        <div><span class="num-correct">${correct}</span><br>out of ${maxQuestions}</div>
-      `;
-
-      resultBox.innerHTML = html;
-
+      updatResultsPage();
       btn.textContent = "Play Again";
     }
 
     else if (btn.textContent == "Play Again") {
       screen = "start";
       questionCount = 0;
-      btn.style.visibility = "hidden";
-      quizHeader.innerHTML = "";
 
       document.querySelectorAll(".box").forEach(box => {
         box.innerHTML = "";
@@ -228,24 +128,97 @@ document.addEventListener("click", e => {
       });
 
       document.querySelector(".box--result").classList.remove("box--result");
+      document.querySelector(".section-info").classList.remove("quiz-select");
+      currentQuestion.style.display = "block";
 
-      quizzes.forEach((quiz, i) => {
-        const box = document.querySelector(`.box--${i+1}`);
-        const html = `
-          <div class="box__image ${quiz["title"].toLowerCase()}"><img src=${quiz["icon"]} alt=${quiz["title"]}></div>
-          <div class="box__text">${quiz["title"]}</div>
-        `;
-
-        questionH1.innerHTML = `
-          Welcome to the<span class="section-info__header--span">Frontend Quiz!</span>
-        `;
-
-        document.querySelector(".section-info").classList.remove("quiz-select");
-        currentQuestion.style.display = "block";
-        currentQuestion.textContent = "Pick a subject to get started";
-      
-        box.innerHTML = html;
-      });
+      populateStartMenu();
     }
   }
 });
+
+
+function populateStartMenu() {
+  btn.style.visibility = "hidden";
+  quizHeader.innerHTML = "";
+  questionH1.innerHTML = `
+    Welcome to the<span class="section-info__header--span">Frontend Quiz!</span>
+  `;
+  currentQuestion.textContent = "Pick a subject to get started";
+
+  quizzes.forEach((quiz, i) => {
+    const box = document.querySelector(`.box--${i+1}`);
+    const html = `
+      <div class="box__image ${quiz["title"].toLowerCase()}"><img src=${quiz["icon"]} alt=${quiz["title"]}></div>
+      <div class="box__text">${quiz["title"]}</div>
+    `;
+  
+    box.innerHTML = html;
+  });
+}
+
+function updateQuestionPage(question) {
+  options = question["options"];
+  questionH1.classList.add("quiz-question");
+  questionH1.textContent = question["question"];
+  currentQuestion.innerHTML = `Question ${questionCount + 1} of ${maxQuestions}`;
+  progressValue.style.width = `${((questionCount+1)/maxQuestions) * 100}%`; 
+  displayOptions(options);
+}
+
+function displayOptions(options) {
+  options.forEach((option, i) => {
+    const box = document.querySelector(`.box--${i+1}`);
+    const html = `
+      <div class="box__image box__image--option">${optionsLetters[i]}</div>
+      <div class="box__text">${escapeHTML(option)}</div>
+      <img class="result result--correct" src="./assets/images/icon-correct.svg">
+      <img class="result result--error" src="./assets/images/icon-error.svg">
+    `;
+
+    box.innerHTML = html;
+  });
+}
+
+function updatResultsPage() {
+  document.querySelector(".selected").classList.remove("correct", "error", "selected");
+
+  document.querySelector(".progress").style.display = "none";
+  currentQuestion.style.display = "none";
+  questionH1.innerHTML = `
+    Quiz completed<span class="section-info__header--span">You scored...</span>
+  `;
+  questionH1.classList.remove("quiz-question");
+
+  document.querySelectorAll(".box").forEach(box => {
+    box.innerHTML = "";
+    box.style.display = "none";
+  });
+
+  const resultBox = document.querySelector(".box--1");
+  resultBox.style.display = "flex";
+  resultBox.classList.add("box--result");
+  
+  const html = `
+    <div class="quiz-header">
+      <img src="${quiz["icon"]}" class="${quiz["title"].toLowerCase()}">
+      <span>${quiz["title"]}</span>
+    </div>
+    <div><span class="num-correct">${correct}</span><br>out of ${maxQuestions}</div>
+  `;
+
+  resultBox.innerHTML = html;
+}
+
+
+const escapeHTML = str =>
+  str.replace(
+    /[&<>'"]/g,
+    tag =>
+      ({
+        '&': '&amp;',
+        '<': '&lt;',
+        '>': '&gt;',
+        "'": '&#39;',
+        '"': '&quot;'
+      }[tag] || tag)
+  );
