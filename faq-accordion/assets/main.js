f6300909f8cd2cf40f11a88ff26a322a2c0e91df
vcollins1 <vontrellecollins@gmail.com>
2024-07-14 09:36:13 -0400

    accessibility improvements

--- faq-accordion/assets/main.js
+++ faq-accordion/assets/main.js
@@ -4,13 +4,16 @@ document.addEventListener("click", e => {
     const panel = document.querySelector(`.p${panelNum}`);
     const panelImg = document.querySelector(`.accordion__img.accordion--${panelNum}`);
     panel.classList.toggle("show");
+    const btn = document.querySelector(`#btn${panelNum}`);
     
     if (panel.classList.contains("show")) {
       panelImg.src = "./assets/images/icon-plus.svg";
       panelImg.alt = "plus icon";
+      btn.setAttribute("aria-expanded", false);
     } else {
       panelImg.src = "./assets/images/icon-minus.svg";
       panelImg.alt = "minus icon";
+      btn.setAttribute("aria-expanded", true);
     }
   }
 })
