f6b7ea882838ed82aa5c772a3de54807252ffda4
vcollins1 <vontrellecollins@gmail.com>
2024-08-02 06:23:01 -0400

    mobile menu functionality

---
+++ ecommerce-product-page/assets/main.js
@@ -0,0 +1,15 @@
+document.addEventListener("click", e => {
+  if (e.target.classList.contains("menu-icon")) {
+    const mainNav = document.querySelector(".main-nav");
+
+    if (mainNav.classList.contains("open")) {
+      mainNav.classList.remove("open");
+      e.target.classList.remove("active");
+      e.target.src = "./assets/images/icon-menu.svg";
+    } else {
+      mainNav.classList.add("open");
+      e.target.classList.add("active");
+      e.target.src = "./assets/images/icon-close.svg";
+    }
+  }
+});
