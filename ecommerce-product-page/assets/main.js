7865d8e5d18177ddfaa05084aefa5b759f7d99ae
vcollins1 <vontrellecollins@gmail.com>
2024-08-12 14:38:06 -0400

    image light box

--- ecommerce-product-page/assets/main.js
+++ ecommerce-product-page/assets/main.js
@@ -11,6 +11,47 @@ let quantityNum = 0;
 
 
 document.addEventListener("click", e => {
+  const activeThumbnails = document.querySelectorAll(".active-thumbnail");
+
+  
+  if (e.target.classList.contains("image-slider__image")) {
+    const sliderClone = document.querySelector(".image-slider").cloneNode(true);
+
+    const shadowBody = document.createElement("div");
+    shadowBody.classList.add("shadow-body");
+
+    const shadow = document.createElement("div");
+    shadow.classList.add("shadow");
+
+    shadow.innerHTML = `
+      <svg width="14" height="15" xmlns="http://www.w3.org/2000/svg" class="shadow-close">
+        <path class="shadow-close" d="m11.596.782 2.122 2.122L9.12 7.499l4.597 4.597-2.122 2.122L7 9.62l-4.595 4.597-2.122-2.122L4.878 7.5.282 2.904 2.404.782l4.595 4.596L11.596.782Z" fill="#69707D" fill-rule="evenodd"/>
+      </svg>`;
+    shadow.appendChild(sliderClone);
+    shadowBody.appendChild(shadow);
+
+    document.body.append(shadowBody);
+  }
+
+  if (e.target.classList.contains("thumbnails__image")) {
+    const currentThumbnail = e.target.parentElement
+    currentImage = currentThumbnail.classList[1].slice(-1) - 1;
+
+    activeThumbnails.forEach(item => {
+      item.classList.remove("active-thumbnail");
+    });
+    
+    document.querySelectorAll(`.thumbnails__box--${currentImage + 1}`).forEach(item => {
+      item.classList.add("active-thumbnail");
+    });
+
+  }
+
+  if (e.target.classList.contains("shadow-close")) {
+    const shadowBody = document.querySelector(".shadow-body");
+    document.body.removeChild(shadowBody);
+  }
+
   if (e.target.classList.contains("menu-icon")) {
     const mainNav = document.querySelector(".main-nav");
 
@@ -32,14 +73,39 @@ document.addEventListener("click", e => {
   }
 
   const sliderImage = document.querySelector(".image-slider__image");
+  const sliderImageShadow = document.querySelector(".shadow .image-slider__image");
   if (e.target.classList.contains("next")) {
     currentImage = ++currentImage % sliderImages.length;
     sliderImage.src = `${sliderImages[currentImage]}`;
+
+    activeThumbnails.forEach(item => {
+      item.classList.remove("active-thumbnail");
+    });
+
+    document.querySelectorAll(`.thumbnails__box--${currentImage + 1}`).forEach(item => {
+      item.classList.add("active-thumbnail");
+    });
+
+    if (sliderImages != null) {
+      sliderImageShadow.src = `${sliderImages[currentImage]}`;
+    }
   }
 
   if (e.target.classList.contains("previous")) {
     currentImage = currentImage == 0 ? 3 : --currentImage;
     sliderImage.src = `${sliderImages[currentImage]}`;
+
+    activeThumbnails.forEach(item => {
+      item.classList.remove("active-thumbnail");
+    });
+
+    document.querySelectorAll(`.thumbnails__box--${currentImage + 1}`).forEach(item => {
+      item.classList.add("active-thumbnail");
+    });
+
+    if (sliderImages != null) {
+      sliderImageShadow.src = `${sliderImages[currentImage]}`;
+    }
   }
 
   const quantity = document.querySelector(".quantity");
