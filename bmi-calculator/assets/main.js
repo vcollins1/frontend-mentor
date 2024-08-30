7ccfb249265c36203ac229fd13736fb075e71fd9
Vontrelle Collins <149267957+vcollins1@users.noreply.github.com>
2024-08-30 16:41:20 -0400

    change between metric and imperial display

--- bmi-calculator/assets/main.js
+++ bmi-calculator/assets/main.js
@@ -1,9 +1,67 @@
-const resultsBMI = document.querySelector(".results__bmi")
+const resultsBMI = document.querySelector(".results__bmi");
+const bmiInputs = document.querySelector(".bmi-calc__inputs");
+let currentUnits = "metric";
 
 
 document.addEventListener("change", e => {
+    if (e.target.classList.contains("bmi-calc__units--radio-input")) {
+        resultsBMI.innerHTML = "0";
+        currentUnits = e.target.classList[1];
+        bmiInputs.classList.toggle("imperial");
+
+        if (currentUnits === "imperial") {
+            bmiInputs.innerHTML = `
+                <label class="user-input">
+                    <span class="description">Height</span>
+                    <span class="input">
+                        <input type="number" class="input__field height-ft" min="0">
+                        <span class="input__unit">ft</span>
+                    </span>
+
+                    <span class="input">
+                        <input type="number" class="input__field height-in" min="0">
+                        <span class="input__unit">in</span>
+                    </span>
+                </label>
+                
+                <label class="user-input">
+                    <span class="description">Weight</span>
+                    <span class="input">
+                        <input type="number" class="input__field weight-st" min="0">
+                        <span class="input__unit">st</span>
+                    </span>
+
+                    <span class="input">
+                        <input type="number" class="input__field weight-lbs" min="0">
+                        <span class="input__unit">lbs</span>
+                    </span>
+                </label>
+            `;
+        } else {
+            bmiInputs.innerHTML = `
+                <label class="user-input">
+                    <span class="description">Height</span>
+                    <span class="input">
+                        <input type="number" class="input__field height-cm" min="0">
+                        <span class="input__unit">cm</span>
+                    </span>
+                </label>
+                
+                <label class="user-input">
+                    <span class="description">Weight</span>
+                    <span class="input">
+                        <input type="number" class="input__field weight-kg" min="0">
+                        <span class="input__unit">kg</span>
+                    </span>
+                </label>    
+            `;
+        }
+    }
+
     if (e.target.classList.contains("input__field")) {
-        resultsBMI.innerHTML = calculateMetricBMI();
+        if (currentUnits === "metric") {
+            resultsBMI.innerHTML = calculateMetricBMI();
+        }
     }
 })
 
